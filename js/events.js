7e132b9fb08daa13f80261f098ed3acbdcf53cb0
Matthew Hunger <matt.hunger@gmail.com>
2017-09-16 10:13:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -29,10 +29,10 @@ function submitIt() {
   /*$("form").on("submit", function() {
     if ($( "input:first" ).val()==="correct") {
         alert('Your form is going to be submitted now.');
-        return;*/
+        return;
     }
   })
-
+*/
 }
 
 $(document).ready(function(){
